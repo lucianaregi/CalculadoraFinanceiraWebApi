ab77775de124d0a74d05cea9afe6a76e6ad80c94
lucianaregi <lucianaregi@gmail.com>
2021-02-08 15:50:03 -0300

    Criado o projeto de testes com xUnit.

---
+++ src/TaxaJurosAPI/Testes/TaxaJurosxUnitTest/TaxaJurosUnitTestController.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaxaJuros.API.Controllers;
+using TaxaJuros.API.Service;
+using Xunit;
+
+namespace TaxaJurosxUnitTest
+{
+    public class TaxaJurosUnitTestController
+    {
+        private readonly TaxaJurosService taxaJurosService;
+
+        [Fact]
+        public void GetTaxaJurosWait()
+        {
+            GetTaxaJurosAsync().Wait();
+        }
+
+        [Fact]
+        public async void TestFailedValue()
+        {
+            // arrange
+            var juros = new TaxaJurosService();
+
+            // act
+            var data = await juros.GetTaxaJuros();
+
+            Assert.Equal("0.01", data.ToString());
+            
+        }
+
+        [Fact]
+        public async void TestSuccessValue()
+        {
+            // arrange
+            var juros = new TaxaJurosService();
+
+            // act
+            var data = await juros.GetTaxaJuros();
+
+            Assert.Equal("0,01", data.ToString());
+
+        }
+
+        private async Task GetTaxaJurosAsync()
+        {
+            // arrange
+            var controller = new TaxaJurosController(taxaJurosService);
+
+            // act
+            var data = await controller.Get();
+
+            // assert
+            Assert.NotNull(data);
+        }
+
+       
+
+    }
+}
