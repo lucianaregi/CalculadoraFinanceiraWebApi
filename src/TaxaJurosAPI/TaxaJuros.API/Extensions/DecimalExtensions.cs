00aec973e7a1546c5b36c6d2c22804cebf70057b
lucianaregi <lucianaregi@gmail.com>
2021-02-08 12:36:23 -0300

    Criado o projeto.

---
+++ src/TaxaJurosAPI/TaxaJuros.API/Extensions/DecimalExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace TaxaJuros.API.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string ToDecimal(this object value)
+        {
+            if (null == value)
+                return null;
+
+            try
+            {
+                return Convert
+                 .ToDecimal(value, CultureInfo.InvariantCulture)
+                 .ToString("N", CultureInfo.GetCultureInfo("pt-BR"));
+            }
+            catch (FormatException)
+            {
+                return "0,00";
+            }
+        }
+    }
+}
---
+++ src/TaxaJurosAPI/TaxaJuros.API/Service/TaxaJurosService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace TaxaJuros.API.Service
+{
+    public class TaxaJurosService
+    {
+        public Task<decimal> GetTaxaJuros() => Task.FromResult(0.01M);
+    }
+}
