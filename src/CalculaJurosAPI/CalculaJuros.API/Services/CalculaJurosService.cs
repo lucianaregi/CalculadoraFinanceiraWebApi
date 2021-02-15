e9433c44182c9fe14c696662827e73ca89db6ab0
lucianaregi <lucianaregi@gmail.com>
2021-02-15 20:20:05 -0300

    Adicionado o dockerfile e criado o método para acessar a api taxa de juros.

--- src/CalculaJurosAPI/CalculaJuros.API/Services/CalculaJurosService.cs
+++ src/CalculaJurosAPI/CalculaJuros.API/Services/CalculaJurosService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using TaxaJuros.API.Extensions;
 
@@ -10,14 +11,43 @@ namespace CalculaJuros.API.Services
     {
         public async Task<decimal> CalcularJurosAsync(decimal valorInicial, int meses)
         {
-            //var taxaJuros = await _api1Service.ObterTaxaDeJurosAsync();
-            var taxaJuros = 0.01m;
+            var taxaJuros = await ObterTaxaDeJurosAsync();
+            //var taxaJuros = 0.01m;
             var potencia = (decimal)Math.Pow(1 + ((double)taxaJuros), meses);
             var valorCalculado = Convert.ToDecimal(valorInicial * potencia);
             var valorFinal = Math.Round(valorCalculado, 2);
             return valorFinal;
 
 
+        }
+
+        public  async Task<decimal> ObterTaxaDeJurosAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = "http://localhost:8000/api/taxaJuros";
+            string resultado = string.Empty;
+                
+            try
+            {
+                Uri _url = new Uri(url);
+                var response = await httpClient.GetAsync(_url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultado = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    resultado = response.StatusCode.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                var mensagem = ex.Message;
+                return 0;
+            }
+
+            return decimal.Parse(resultado);
         }
     }
 }
